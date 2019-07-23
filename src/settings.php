39c4d43288687d18c82b2cf5be7b5f80d740e6e5
Adrien Toussaint <adrien.toussaint@compta-clementine.fr>
2019-07-23 15:32:25 +0200

    Révision css

--- src/settings.php
+++ src/settings.php
@@ -2,19 +2,19 @@
 
     $dbconfig = [
         'driver'    => 'mysql',
-        'host'      => 'localhost:3306',
+        'host'      => 'localhost:8889',//localhost:3306
         'database'  => 'musee',
-        'username'  => 'admin',
-        'password'  => 'admin'
+        'username'  => 'root',//admin
+        'password'  => 'root'//admin
     ];
 
 if (isset($_SERVER["HTTP_HOST"]) && $_SERVER["HTTP_HOST"] == 'localhost') {
     $dbconfig = [
         'driver'    => 'mysql',
-        'host'      => 'localhost:3306',
+        'host'      => 'localhost:8889',//localhost:3306
         'database'  => 'musee',
-        'username'  => 'admin',
-        'password'  => 'admin'
+        'username'  => 'root',//admin
+        'password'  => 'root'//admin
     ];
 }
 
